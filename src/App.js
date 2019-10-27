daa9ccfc9ff122858e0e83438f101dafccfebe90
ii02735 <muhammad.yadallee1@etu.parisdescartes.fr>
2019-10-27 20:32:54 +0100

    JSX principles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return ( //JSX (not really HTML) content above :
+//     <div className="App">
+//         <h1>Hi I'm a React application</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component
+{
+  render()
+  {
+    return ( //JSX (not really HTML) content above :
+              //As an exemple : we write "class" attribute in HTML, but here "className"
+        <div className="Hello">
+          <h1>Hi I'm a React application</h1>
+       </div>
+       //warning : any HTML-ish JSX code must be nested inside a ROOT element
+   );
+   //Is equivalent to :
+   //return React.createElement("div",{className: "App"},React.createElement("h1",null,"Hi I'm a React application"));
+  }
 }
 
-export default App;
+export default App; //export App Class to be used somewhere
